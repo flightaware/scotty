85022ae752efdab38d474cd79dfd212919e76be7
Peter da Silva <peter.dasilva@flightaware.com>
2017-03-06 19:41:26 +0000

    Hostnames ending in '.' are allowed by DNS.

--- tnm/generic/tnmUtil.c
+++ tnm/generic/tnmUtil.c
@@ -1095,8 +1095,11 @@ TnmValidateIpHostName(Tcl_Interp *interp, const char *name)
     /*
      * A host name must start with one of the characters [a-zA-Z0-9]
      * and continue with characters from the set [-.a-zA-Z0-9] and
-     * must not end with a '.' or a '-'. Names that only contain
+     * must not end with a '-'. Names that only contain
      * digits and three dots are also not allowed.
+     *
+     * NOTE: a hostname is allowed to end with a dot, which the previous version of this code
+     * explicitly disallowed.
      */
 
     if (! isalnum(*p)) {
@@ -1109,7 +1112,7 @@ TnmValidateIpHostName(Tcl_Interp *interp, const char *name)
 	last = *p++;
     }
 
-    if (*p == '\0' && isalnum(last) && (alpha || dots != 3)) {
+    if (*p == '\0' && (isalnum(last) || last == '.') && (alpha || dots != 3)) {
 	return TCL_OK;
     }
 
